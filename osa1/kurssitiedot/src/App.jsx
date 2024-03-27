5f1765b550f4f64317f0728f136732fea40ee500
Tamás Tóth <tomjtoth@gmail.com>
2024-03-27 17:56:57 +0200

    1.4 done

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -16,7 +16,11 @@ const Content = ({parts: [p1,p2,p3]}) => (
   </>
 )
 
-const Total = ({ex1,ex2,ex3}) => (
+const Total = ({parts:[
+  {exercises: ex1},
+  {exercises: ex2},
+  {exercises: ex3}
+]}) => (
   <p>
     Number of exercises {ex1 + ex2 + ex3}
   </p>
@@ -24,24 +28,26 @@ const Total = ({ex1,ex2,ex3}) => (
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course}></Header>
-      <Content parts={[part1,part2,part3]}></Content>
-      <Total ex1={part1.exercises} ex2={part2.exercises} ex3={part3.exercises}></Total>
+      <Content parts={parts}></Content>
+      <Total parts={parts}></Total>
     </div>
   )
 }
