3067eb7cfd1bae185f4fd6243bceb215db592164
Tamás Tóth <tomjtoth@gmail.com>
2024-03-28 20:58:41 +0200

    case insensitive filter

--- osa2/puhelinluettelo/src/components/Persons.jsx
+++ osa2/puhelinluettelo/src/components/Persons.jsx
@@ -1,16 +1,19 @@
-const Persons = ({ x: { persons, filter } }) => (
-    <ul>
-        {(
-            filter === ''
+const Persons = ({ x: { persons, filter } }) => {
+
+    const lc_filter = filter.toLowerCase();
+
+    return (
+        <ul>
+            {(filter === ''
                 ? persons
                 : persons.filter(({ name, num }) =>
-                    name.includes(filter) || num.includes(filter))
-        ).map(({ name, num }, idx) => (
-            <li key={idx}>
-                {name} {num}
-            </li>
-        ))}
-    </ul>
-)
-
+                    name.toLowerCase().includes(lc_filter) || num.includes(filter))
+            ).map(({ name, num }, idx) => (
+                <li key={idx}>
+                    {name} {num}
+                </li>
+            ))}
+        </ul>
+    )
+}
 export default Persons
