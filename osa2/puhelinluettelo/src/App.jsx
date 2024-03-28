bfe398abe6e8c3ff072f5056bc051b39fd102147
Tamás Tóth <tomjtoth@gmail.com>
2024-03-28 20:10:01 +0200

    2.6 done

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({
+      name: newName
+    }))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name:
+          <input
+            value={newName}
+            onChange={handleNameChange} />
+
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(({ name }, idx) => (
+          <li key={idx}>
+            {name}
+          </li>
+        ))}
+      </ul>
+
+    </div>
+  )
+
+}
+
+export default App
