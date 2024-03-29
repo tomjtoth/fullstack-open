a5824b55ac7e52f05ae9bd403b199b40e212f993
Tamás Tóth <tomjtoth@gmail.com>
2024-03-29 19:25:14 +0200

    3.8 done

--- osa3/puhelinluettelon backend/index.js
+++ osa3/puhelinluettelon backend/index.js
@@ -31,7 +31,16 @@ let max_id = 4;
 //Math.max(...persons.map(({ id }) => id));
 
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        JSON.stringify(req.body)
+    ].join(' ')
+}));
 
 app.get('/info', (_req, resp) => {
     resp.send(`
