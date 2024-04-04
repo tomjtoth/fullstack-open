56cdfbae29bf84cdcf5c0da2d3c2b33f32842eac
Tamás Tóth <tomjtoth@gmail.com>
2024-04-04 15:09:05 +0300

    4.4 done

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,4 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (_blogs) => 1;
 
-module.exports = { dummy };
+const totalLikes = (blogs) => blogs.length === 0
+    ? 0
+    : blogs
+        .map(blog => blog.likes)
+        .reduce((prev, curr) => prev + curr);
+
+module.exports = { dummy, totalLikes };
