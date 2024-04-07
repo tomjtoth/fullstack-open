1d381fb027cf4e8c42c393a27c797adaefda6c06
Tamás Tóth <tomjtoth@gmail.com>
2024-04-07 07:47:54 +0300

    `npm i express-async-errors`

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,5 +1,6 @@
 const { MONGODB_URI } = require('./utils/config');
 const express = require('express');
+require('express-async-errors');
 const app = express();
 const cors = require('cors');
 const router = require('./controllers/blogs');
