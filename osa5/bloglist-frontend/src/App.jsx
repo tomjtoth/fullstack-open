6b01e63b384ce3595eca50369692db6b61cbee00
Tamás Tóth <tomjtoth@gmail.com>
2024-04-11 10:11:51 +0300

    5.1 done

---
+++ osa5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import blogService from './services/blogs'
+import BlogForm from './components/BlogForm'
+import LoginForm from './components/LoginForm'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs(blogs)
+    )
+  }, [])
+
+
+  return (<>
+    {user
+      ? <BlogForm x={{ blogs, user }} />
+      : <LoginForm x={{
+        username, setUsername,
+        password, setPassword,
+        setUser, setErrorMessage
+      }} />
+    }
+  </>
+  )
+}
+
+export default App
