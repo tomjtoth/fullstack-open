f04636130e1ab34a71206a4596d3b58d8a31b142
Tamás Tóth <tomjtoth@gmail.com>
2024-04-12 10:16:03 +0300

    5.12 done

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,34 +1,39 @@
-import { useState } from "react"
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const Blog = ({ x: [blog, incrLike, delBlog] }) => {
-  const [visible, setVisible] = useState(false)
+    const [visible, setVisible] = useState(false);
 
-  return (
-    <li>
-      {blog.title} {blog.author}
-      <button onClick={() => setVisible(!visible)}>{
-        visible
-          ? 'collapse'
-          : 'expand'
-      }</button>
-      {visible && <ul>
+    return (
         <li>
-          {blog.url}
-        </li>
-        <li>
+            {blog.title} {blog.author}
+            <button onClick={() => setVisible(!visible)}>{
+                visible
+                    ? 'collapse'
+                    : 'expand'
+            }</button>
+            {visible && <ul>
+                <li>
+                    {blog.url}
+                </li>
+                <li>
           likes {blog.likes}
-          <button
-            onClick={incrLike}
-          >like</button>
-        </li>
-        <li>
+                    <button
+                        onClick={incrLike}
+                    >like</button>
+                </li>
+                <li>
           added by {blog.user.name}
+                </li>
+                {delBlog && <button onClick={delBlog}>remove this</button>}
+            </ul>
+            }
         </li>
-        {delBlog && <button onClick={delBlog}>remove this</button>}
-      </ul>
-      }
-    </li>
-  )
-}
+    );
+};
 
-export default Blog
+Blog.propTypes = {
+    x: PropTypes.array.isRequired
+};
+
+export default Blog;
