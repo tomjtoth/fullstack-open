f19c22562c18c3a3a9dc2911b9ad8b4e4aee8d30
Tamás Tóth <tomjtoth@gmail.com>
2024-04-16 09:00:18 +0300

    6.22: merged back things to App.jsx, creating/voting anecdote works now

--- osa6/query-anecdotes/src/App.jsx
+++ osa6/query-anecdotes/src/App.jsx
@@ -1,10 +1,52 @@
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './services/anecdotes'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, createAnecdote, updateAnecdote } from './services/anecdotes'
 import Notification from './components/Notification'
-import AnecdoteForm from './components/AnecdoteForm'
-import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const newAnMut = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAn) => {
+      queryClient.invalidateQueries('anecdotes')
+      // const anecdotes = queryClient.getQueryData('anecdotes')
+      // queryClient.setQueryData(
+      //   { queryKey: ['anecdotes'] },
+      //   anecdotes.concat(newAn)
+      // )
+    },
+  })
+
+  const onCreate = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    newAnMut.mutate({ content, votes: 0 },)
+  }
+
+  const voteAnMut = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (updatedAn) => {
+      queryClient.invalidateQueries('anecdotes')
+      // const anecdotes = queryClient.getQueryData('anecdotes')
+      // queryClient.setQueryData(
+      //   { queryKey: ['anecdotes'] },
+      //   anecdotes.map(localAn =>
+      //     localAn.id === updatedAn.id
+      //       ? updatedAn
+      //       : localAn
+      //   )
+      // )
+    },
+  })
+
+  const handleVote = (anecdote) => {
+    voteAnMut.mutate({
+      ...anecdote,
+      votes: anecdote.votes + 1
+    })
+  }
+
 
   const result = useQuery({
     queryKey: ['anecdotes'],
@@ -21,14 +63,34 @@ const App = () => {
     return <div>server error</div>
   }
 
+  const anecdotes = result.data
+
   return (
     <div>
       <h3>Anecdote app</h3>
 
       <Notification />
-      <AnecdoteForm />
-      <AnecdoteList />
-    </div>
+
+      <div>
+        <h3>create new</h3>
+        <form onSubmit={onCreate}>
+          <input minLength="5" name='anecdote' />
+          <button type="submit">create</button>
+        </form>
+      </div>
+      {
+        anecdotes.map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => handleVote(anecdote)}>vote</button>
+            </div>
+          </div>
+        )
+      }    </div>
   )
 }
 
