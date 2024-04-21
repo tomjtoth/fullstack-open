ad350c7ce73e20dc77431206557b230fb5ab6dd9
Tamás Tóth <tomjtoth@gmail.com>
2024-04-21 13:32:14 +0300

    8.19: filter by genres via radiobuttons

--- osa8/library-frontend/src/components/Books.jsx
+++ osa8/library-frontend/src/components/Books.jsx
@@ -1,15 +1,29 @@
+import { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries';
 
 const Books = ({ page }) => {
 
-  const result = useQuery(ALL_BOOKS, {
+  const [books, setBooks] = useState([]);
+  const [genre, setGenre] = useState('');
+
+  const sg = (val) => () => setGenre(val);
+
+  const booksResult = useQuery(ALL_BOOKS, {
     // pollInterval: 2000
   });
 
+  useEffect(() => {
+    if (booksResult.data) {
+      if (booksResult.data.allBooks === null) return
+
+      setBooks([...booksResult.data.allBooks])
+    }
+  }, [booksResult.data])
+
   return page !== 'books'
     ? null
-    : (result.loading
+    : (booksResult.loading
       ? <div>loading...</div>
       : (
         <div>
@@ -22,15 +36,37 @@ const Books = ({ page }) => {
                 <th>author</th>
                 <th>published</th>
               </tr>
-              {result.data && result.data.allBooks.map((a) => (
-                <tr key={a.title}>
-                  <td>{a.title}</td>
-                  <td>{a.author.name}</td>
-                  <td>{a.published}</td>
-                </tr>
-              ))}
+              {books
+                .filter(b => genre === '' || b.genres.includes(genre))
+                .map((a) => (
+                  <tr key={a.title}>
+                    <td>{a.title}</td>
+                    <td>{a.author.name}</td>
+                    <td>{a.published}</td>
+                  </tr>
+                ))}
             </tbody>
           </table>
+          <h3>filter by genre</h3>
+          <label htmlFor="default-genre">all</label>
+          <input type="radio" value="" checked={genre === ''}
+            name="genre"
+            id="default-genre"
+            onClick={sg('')} />
+          {[...new Set(books
+            .map(({ genres }) => genres)
+            .flat())
+          ].sort().map(g => {
+
+            const id = `rbtn-${g}`
+
+            return <div key={g}>
+              <label htmlFor={id}>{g}</label>
+              <input type="radio" name="genre" id={id}
+                onClick={sg(g)}
+                checked={genre === g} />
+            </div>
+          })}
         </div>
       )
     );
--- osa8/library-frontend/src/queries.js
+++ osa8/library-frontend/src/queries.js
@@ -8,6 +8,7 @@ const ALL_BOOKS = gql`
         name
       }
       published
+      genres
     }
   }
 `;
